c0d8c77c0eab2150d1a2cc92170865c4d0b268e6
ROSARIO <ROSARIO@DESKTOP-M6A4MQP>
2023-04-20 21:57:00 -0300

    inicializacion del proyecto. martinez

---
+++ src/main/java/ar/edu/unju/edm/Tp2MartinezApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.edm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp2MartinezApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp2MartinezApplication.class, args);
+	}
+
+}
